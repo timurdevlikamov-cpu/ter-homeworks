c6d6ad55a827469676a905433fe90a29dea194c9
timurdevlikamov-cpu <timur.devlikamov@gmail.com>
2026-06-22 19:31:06 +0300

    Update vms_platform.tf

--- 02/vms_platform.tf
+++ 02/vms_platform.tf
@@ -1,82 +1,106 @@
-###cloud vars
+### vm_web vars
 
-
-variable "cloud_id" {
+variable "vm_web_name" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default     = "netology-develop-platform-web"
 }
 
-variable "folder_id" {
+variable "vm_web_image_family" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default     = "ubuntu-2004-lts"
 }
 
-variable "default_zone" {
+variable "vm_web_platform_id" {
   type        = string
-  default     = "ru-central1-b"
-  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
-}
-variable "default_cidr" {
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+  default     = "standard-v3"
 }
 
-variable "vpc_name" {
-  type        = string
-  default     = "develop2"
-  description = "VPC network & subnet name"
+#variable "vm_web_cores" {
+#  type        = number
+#  default     = 2
+#}
+
+#variable "vm_web_memory" {
+#  type        = number
+#  default     = 1
+#}
+
+#variable "vm_web_core_fraction" {
+#  type        = number
+#  default     = 20
+#}
+
+variable "vm_web_preemptible" {
+  type        = bool
+  default     = true
 }
 
-
-###ssh vars
-
-variable "vms_ssh_public_root_key" {
-  type        = string
-  default     = "ssh-rsa <my_key>"
-  description = "ssh-keygen -t ed25519"
-}
-
-### vm_web vars (..... ..........)
+### vm_db vars
 
 variable "vm_db_name" {
   type        = string
   default     = "netology-develop-platform-db"
-  description = "Name of the VM"
 }
 
 variable "vm_db_image_family" {
   type        = string
   default     = "ubuntu-2004-lts"
-  description = "OS image family"
 }
 
 variable "vm_db_platform_id" {
   type        = string
   default     = "standard-v3"
-  description = "Platform ID"
 }
 
-variable "vm_db_cores" {
-  type        = number
-  default     = 2
-  description = "Number of CPU cores"
-}
+#variable "vm_db_cores" {
+#  type        = number
+#  default     = 2
+#}
 
-variable "vm_db_memory" {
-  type        = number
-  default     = 2
-  description = "Amount of RAM in GB"
-}
+#variable "vm_db_memory" {
+#  type        = number
+#  default     = 2
+#}
 
-variable "vm_db_core_fraction" {
-  type        = number
-  default     = 20
-  description = "Core fraction"
-}
+#variable "vm_db_core_fraction" {
+#  type        = number
+#  default     = 20
+#}
 
 variable "vm_db_preemptible" {
   type        = bool
   default     = true
-  description = "Whether the instance is preemptible"
 }
+
+variable "vm_db_zone" {
+  type    = string
+  default = "ru-central1-b"
+}
+
+variable "vms_resources" {
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+  }))
+  default = {
+    web = {
+      cores         = 2
+      memory        = 1
+      core_fraction = 20
+    }
+    db = {
+      cores         = 2
+      memory        = 2
+      core_fraction = 20
+    }
+  }
+}
+
+#variable "common_metadata" {
+#  type = map(string)
+#  default = {
+#    "serial-port-enable" = "1"
+#    "ssh-keys"           = "ubuntu:${var.vms_ssh_public_root_key}"
+#  }
+#}
