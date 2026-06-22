fb5829f091d2e6f0ab0bca08de3d827c52c44da3
timurdevlikamov-cpu <timur.devlikamov@gmail.com>
2026-06-22 19:52:40 +0300

    Update locals.tf

---
+++ 02/src/locals.tf
@@ -0,0 +1,11 @@
+locals {
+
+  web_vm_name = "${var.vpc_name}-${var.vm_web_name}"
+  db_vm_name  = "${var.vpc_name}-${var.vm_db_name}"
+
+  common_metadata = {
+    "serial-port-enable" = "1"
+    "ssh-keys"           = "ubuntu:${var.vms_ssh_public_root_key}"
+  }
+
+}
